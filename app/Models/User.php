99f55b77c2ff6bcfc60df7b0ba873e6b1644e8e6
Jonathan Gawrych <jonathan@gawrych.us>
2021-09-28 18:30:13 -0600

    Add basic User model/migration

---
+++ app/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $id
+ * @property string $lower
+ * @property string $name
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon $deleted_at
+ */
+class User extends Model
+{
+    use HasFactory, SoftDeletes;
+}
